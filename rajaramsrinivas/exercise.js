c6c3cfa00b85909113917182ef9a6ff2ae1c9644
rajaramsrinivas <rajaram.1990@gmail.com>
2018-03-14 10:12:29 -0400

    Updated exercise file(s)

--- rajaramsrinivas/exercise.js
+++ rajaramsrinivas/exercise.js
@@ -100,37 +100,6 @@ quiz.question_06 = function(data, carName, model, doors, color) {
 
   var carPrice = 0;
   // TODO your code here
-  var carData= {
-    "cars": {
-        "Nissan": [
-            {"model":"Sentra", "doors":4, "price": 17000},
-            {"model":"Maxima", "doors":4,  "price": 18000},
-            {"model":"GTR", "doors":2,  "price": 210000},
-            {"model":"Altima", "doors":2,  "price": 23300},
-            {"model":"LEAF", "doors":2,  "price": 29900},
-            {"model":"Maxima", "doors":2,  "price": 33270},
-            {"model":"Versa", "doors":2,  "price": 12110},
-            {"model":"Juke", "doors":2,  "price": 20900}
-        ],
-        "Ford": [
-            {"model":"Taurus", "doors":4, "price": 21000},
-            {"model":"Escort", "doors":4,  "price": 22000},
-            {"model":"Expedition", "doors":4,  "price": 64447},
-            {"model":"Fusion", "doors":4,  "price": 24000},
-            {"model":"Fiesta ", "doors":4,  "price": 13995},
-            {"model":"EcoSport ", "doors":4,  "price": 20995},
-            {"model":"Edge ", "doors":4,  "price": 30215},
-            {"model":"Explorer ", "doors":4,  "price": 32985}
-        ],
-        "BMW": [
-            {"model":"series 3", "doors":4, 
-                "color":[ 
-                {"id":"red", "price": 35000 },
-                {"id":"silver", "price": 40000 }]
-            }
-        ]
-      }
-    }
     var modelsArray = data[carName];
     var filterModel = function(element, i, array) {
       return element.model == model;
@@ -249,12 +218,12 @@ quiz.question_11 = function(input) {
   // Eample output: 'Bob, Sue'
   // ---------------------------------------------------------------
 
-  var res = input.reduce(
-    function(/* TODO args */) {
+  //var res = input.reduce(
+  // function(/* TODO args */) {
       // TODO your code here
-    },
-    0 /* TODO set correct starting value */,
-  );
+  //  },
+  //  0 /* TODO set correct starting value */,
+  //);*/
   return res;
 };
 
